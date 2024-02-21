e61185f43a018c1f073060c371d4b20affa5e4d2
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-21 17:13:59 +0300

    add Brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,26 @@
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="brand section__padding">
+      <div>
+        <img src={google} />
+      </div>
+      <div>
+        <img src={slack} />
+      </div>
+      <div>
+        <img src={atlassian} />
+      </div>
+      <div>
+        <img src={dropbox} />
+      </div>
+      <div>
+        <img src={shopify} />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
