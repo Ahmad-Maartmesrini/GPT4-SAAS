67a3bc063ca3545044462abeb13e099f6854f7fb
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-21 20:22:59 +0300

    what's GPT4

--- src/containers/whatGPT4/WhatGPT4.jsx
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -1,5 +1,35 @@
-const WhatGPT4 = () => {
-  return <div>WhatGPT4</div>;
-};
+import Feature from "../../components/feature/Feature";
+import "./whatGPT4.css";
+
+const WhatGPT4 = () => (
+  <div className="gpt4__whatgpt4 section__margin" id="wgpt4">
+    <div className="gpt4__whatgpt4-feature">
+      <Feature
+        title="What is GPT-4"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+      />
+    </div>
+    <div className="gpt4__whatgpt4-heading">
+      <h1 className="gradient__text">
+        The possibilities are beyond your imagination
+      </h1>
+      <p>Explore the Library</p>
+    </div>
+    <div className="gpt4__whatgpt4-container">
+      <Feature
+        title="Chatbots"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+      />
+      <Feature
+        title="Knowledgebase"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments."
+      />
+      <Feature
+        title="Education"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments."
+      />
+    </div>
+  </div>
+);
 
 export default WhatGPT4;
