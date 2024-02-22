634edbf3791792644614f0ede7724db35de6cf9a
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-22 17:21:26 +0300

    Add Blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,20 @@
-const Article = () => {
-  return (
-    <div>Article</div>
-  )
-}
+import "./article.css";
 
-export default Article
+const Article = ({ title, imgURL, date }) => {
+  return (
+    <div className="article__container">
+      <div className="article__container--img">
+        <img src={imgURL} alt="blog_image" />
+      </div>
+      <div className="article__container--info">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,46 @@
+import "./blog.css";
+import Article from "../../components/article/Article";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="blog section__padding" id="blog">
+      <h1 className="blog__heading gradient__text">
+        A lot is happening, We are blogging about it.
+      </h1>
+      <div className="blog__container">
+        <div className="blog__container--groupA">
+          <Article
+            imgURL={blog01}
+            title="Let's Div Deep In The AI World"
+            date="Feb 22, 2024"
+          />
+        </div>
+        <div className="blog__container--groupB">
+          <Article
+            imgURL={blog02}
+            title="Are We Becoming Robots?"
+            date="Feb 20, 2024"
+          />
+          <Article
+            imgURL={blog03}
+            title="What do You Know About BlockChain?"
+            date="Feb 14, 2024"
+          />
+          <Article
+            imgURL={blog04}
+            title="How Does AI Learn From Your Data"
+            date="Feb 09, 2024"
+          />
+          <Article
+            imgURL={blog05}
+            title="Avoid Losing Your Data With AI"
+            date="Feb 01, 2024"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
