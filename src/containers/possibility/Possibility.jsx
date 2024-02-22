d9e29c75f8f3b8bb66dceda873190a267c42693e
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-22 10:42:59 +0300

    add possibility component

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,26 @@
+import possibilityImage from "../../assets/possibility.png";
+import "./possibility.css";
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
-  )
-}
-
-export default Possibility
+    <div className="possibility section__padding" id="possibility">
+      <div className="possibility__image">
+        <img src={possibilityImage} alt="possibility image" />
+      </div>
+      <div className="possibility__content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
+};
+export default Possibility;
