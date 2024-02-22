f1dab7e6c488f9713fdaffc080e1f94297ce4edc
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-22 11:43:16 +0300

    add CTA card

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,17 @@
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <div className="cta">
+      <div className="cta__content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="cta__btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
